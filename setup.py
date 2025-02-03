1267412da4460486eff3e99144a7b9f65b1344f6
hsxiaobai <hsxb2024@163.com>
2025-02-03 15:39:36 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="rubiks222",
+  version="",
+  packages=find_packages(),
+  author="hsxiaobai",
+  description="",
+  long_description=open("README.md").read(),
+  long_description_content_type="text/markdown",
+  url="https://github.com/hsxiaobai/Rubiks222-Python"
+)
