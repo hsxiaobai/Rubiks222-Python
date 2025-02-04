184a490a009914b2dfae2906b53363c6085f35b7
hsxiaobai <hsxb2024@163.com>
2025-02-05 00:16:07 +0800

    Update cube.py

--- rubiks222/cube.py
+++ rubiks222/cube.py
@@ -1,28 +1,63 @@
-'''
-Copyright (c) 2025 hsxiaobai
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-'''
 from __future__ import annotations
-from ._error import *
+
+class CubeFaceStringLengthError(BaseException):
+    pass
+
+class CubeStringLengthError(BaseException):
+    pass
+
+class CubePiece:
+    '''
+    The class for a piece of the cube face.
+    '''
+    def __init__(self: CubePiece, color: str) -> None:
+        self.color = color
+
+    def __str__(self: CubePiece) -> str:
+        return f"CubePiece color '{self.color}'"
+
+    def __repr__(self: CubePiece) -> str:
+        return f"CubePiece(color='{self.color}')"
+
+class CubeFace:
+    '''
+    The class for the cube face.
+    '''
+    def __init__(self: CubeFace, face_str: str) -> None:
+        try:
+            assert len(face_str) == 9
+        except AssertionError:
+            raise CubeFaceStringLengthError("The length of the cube face string must be 9.")
+        
+        self.pieces = []
+        for idx in range(0, 9, 3):  # Parse the cube face string
+            self.pieces.append([CubePiece(color) for color in face_str[idx:idx+3]])
+
+    def __str__(self: CubeFace) -> str:
+        return f"CubeFace: {self.pieces}"
+    
+    def __repr__(self: CubeFace) -> str:
+        return f"CubeFace(pieces={repr(self.pieces)})"
 
 class Cube:
-  '''
-  The class for the cubes.
-  '''
-  def __init__(self : Cube, cube_str: str) -> None:
-    try:
-      assert len(cube_str) == 54
-    except:
-      raise CubeStringLengthError("The length of the cube string must be 54.")
+    '''
+    The class for the cubes.
+    '''
+    def __init__(self: Cube, cube_str: str) -> None:
+        try:
+            assert len(cube_str) == 54
+        except AssertionError:
+            raise CubeStringLengthError("The length of the cube string must be 54.")
+        
+        self.faces = {}
+        faces = ("U", "D", "F", "B", "L", "R")
+        for idx, face in enumerate(faces):  # Parse the cube string
+            start = idx * 9
+            end = (idx + 1) * 9
+            self.faces[face] = CubeFace(cube_str[start:end])
+    
+    def __str__(self: Cube) -> str:
+        return f"Cube: {self.faces}"
+    
+    def __repr__(self: Cube) -> str:
+        return f"Cube(faces={repr(self.faces)})"
